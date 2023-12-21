c9316b2c7db67e7b8dda4255ecc3164560f41af2
Youssef Abdulaziz <azizy5203@gmail.com>
2023-12-21 17:22:46 +0200

    feat: added font face

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import './App.scss'
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-emerald-600">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-emerald-600">
+        Hello world!
+      </h1>
     </>
   );
 }
