9644902c8e11a68f368a3329b38ff7faf2fcd832
Youssef Abdulaziz <azizy5203@gmail.com>
2023-12-24 04:50:04 +0200

    refactor: added tailwindCss, normaliseCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.scss'
+import Home from "@/views/Home/Home";
+
+import "./App.scss";
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-emerald-600">
-        Hello world!
-      </h1>
+      <Home />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    // All Media Quires Start With Min Width
+    screens: {
+      xs: "350px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1400px",
+      "3xl": "1680px",
+    },
+
+    fontSize: {
+      base: "1rem",
+      xl: [
+        "1.25rem",
+        {
+          lineHeight: "2",
+          fontWeight: "700",
+        },
+      ],
+      "2xl": ["1.5rem", {}],
+      "6xl": [
+        "4rem",
+        {
+          fontWeight: "700",
+        },
+      ],
+    },
+
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
+
+    outlineWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
   },
-  plugins: [],
-}
+};
